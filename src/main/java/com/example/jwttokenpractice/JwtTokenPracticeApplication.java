33ac9e0eda8f36dd6bb435a722120604cdeaf547
jm0s_win <woals1488@naver.com>
2024-03-30 17:00:31 +0900

    init

---
+++ src/main/java/com/example/jwttokenpractice/JwtTokenPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwttokenpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwttokenpractice/JwtTokenPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwttokenpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
