812bb52c7cb0eb1b646bfa0b6f1a4b1e10f1f27d
jm0s_win <woals1488@naver.com>
2024-04-20 18:47:17 +0900

    [Refactor] restructured package and files

--- src/main/java/com/example/jwttokenpractice/jwt/RefreshTokenManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.jwttokenpractice.jwt;
-
-import org.springframework.stereotype.Component;
-
-public interface RefreshTokenManager {
-    void putRefreshToken(String username, String uuid);
-    String getRefreshToken(String username);
-    void removeRefreshToken(String username);
-}
---
+++ src/main/java/com/mirae/commerce/MiraeCommerceApplication.java
@@ -0,0 +1,12 @@
+package com.mirae.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class MiraeCommerceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MiraeCommerceApplication.class, args);
+	}
+}
